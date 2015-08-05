04ecdbdc49448fe039df20f56637f4d980a0f961
Rose Kohn <rose.kohn@gmail.com>
2015-08-05 15:36:44 -0600

    add transaction initialize method.

---
+++ lib/transaction.rb
@@ -0,0 +1,14 @@
+class Transaction
+
+  attr_reader :id, :invoice_id, :credit_card_number, :credit_card_expiration_date, :result, :created_at, :updated_at
+
+  def initialize(row)
+    @id                          = row[:id].to_i
+    @invoice_id                  = row[:invoice_id]
+    @credit_card_number          = row[:credit_card_number]
+    @credit_card_expiration_date = row[:credit_card_expiration_date]
+    @created_at                  = row[:created_at]
+    @updated_at                  = row[:updated_at]
+  end
+
+end
