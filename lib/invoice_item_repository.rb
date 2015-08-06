b0f8f99d90a4945bb95e49b7964a1c2f3ec977d9
Rose Kohn <rose.kohn@gmail.com>
2015-08-06 06:20:22 -0600

    update invoice_item, invoice_item_repo, and add read_data test and invoice_item fixture

---
+++ lib/invoice_item_repository.rb
@@ -0,0 +1,38 @@
+require_relative 'invoice_item'
+require_relative 'repository'
+
+class InvoiceItemRepository
+
+  attr_reader :sales_engine, :records
+
+  include Repository
+
+  def initialize(sales_engine)
+    @sales_engine = sales_engine
+  end
+
+  def read_data(data)
+    @records = data.map do |row|
+      InvoiceItem.new(row)
+    end
+  end
+
+  def find_by_item_id(item_id)
+  end
+
+  def find_by_quantity(quantity)
+  end
+
+  def find_by_unit_price(unit_price)
+  end
+
+  def find_all_by_item_id(item_id)
+  end
+
+  def find_all_by_quantity(quantity)
+  end
+
+  def find_all_by_unit_price(unit_price)
+  end
+
+end
