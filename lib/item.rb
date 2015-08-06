03c38eb241a887373c48494f33d3181bb502cb12
David Stinnette <dastinnette@gmail.com>
2015-08-06 10:32:50 -0600

    Complete initial setup for item class, item repository & item repository test

---
+++ lib/item.rb
@@ -0,0 +1,21 @@
+class Item
+
+  attr_reader :id,
+              :name,
+              :description,
+              :unit_price,
+              :merchant_id,
+              :created_at,
+              :updated_at
+
+  def initialize(row)
+    @id          = row[:id].to_i
+    @name        = row[:name]
+    @description = row[:description]
+    @unit_price  = BigDecimal.new(row[:unit_price])/100
+    @merchant_id = row[:merchant_id].to_i
+    @created_at  = row[:created_at]
+    @updated_at  = row[:updated_at]
+  end
+
+end
